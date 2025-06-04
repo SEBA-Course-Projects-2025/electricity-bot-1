15332b0b0da082d5d9dd1e0b787e033f0009a4a1
Dana Litvak <dlitvakk21@gmail.com>
2025-06-04 18:05:44 +0300

    Project Configuration & Splash Screen
    created electricityBot project; created Splash Screen View (initial screen) which navigates to the start screen - Content View

---
+++ electricityBot/electricityBot/electricityBotApp.swift
@@ -0,0 +1,18 @@
+//
+//  electricityBot.swift
+//  electricityBot
+//
+//  Created by Dana Litvak on 22.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct electricityBot: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashScreenView()
+        }
+    }
+}
+
