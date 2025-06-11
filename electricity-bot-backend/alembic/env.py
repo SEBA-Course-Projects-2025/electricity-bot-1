355c3df8b7333fedeefe54bd56bea4904c21b5f2
olesia-mykhailyshyn <omykhailyshyn@kse.org.ua>
2025-06-11 13:52:29 +0300

    many-to-many realtion ship was added
    devices<->users
    a table for holding unassigned devices was created

--- electricity-bot-backend/alembic/env.py
+++ electricity-bot-backend/alembic/env.py
@@ -2,9 +2,7 @@ from logging.config import fileConfig
 import sys
 import os
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
+from sqlalchemy import engine_from_config, pool
 from alembic import context
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
@@ -14,18 +12,27 @@ from application.models import (
     MeasurementModel,
     DeviceModel,
     UserModel,
-)  # IF I HAVE MORE MODELS, I WILL ADD THEM HERE
+    UnassignedDeviceModel,
+    user_device_association
+)
 
-target_metadata = base.metadata
+from dotenv import load_dotenv
+load_dotenv()
 
 config = context.config
+
+database_url = os.getenv("DATABASE_URL")
+if database_url:
+    config.set_main_option("sqlalchemy.url", database_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+target_metadata = base.metadata
+
 
 def run_migrations_offline() -> None:
-    """Runs migrations in offline mode (without connecting to the DB)."""
+    """Run migrations in 'offline' mode."""
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -33,25 +40,22 @@ def run_migrations_offline() -> None:
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
     )
-
     with context.begin_transaction():
         context.run_migrations()
 
 
 def run_migrations_online() -> None:
-    """Runs migrations in online mode (with a connection to the DB)."""
+    """Run migrations in 'online' mode."""
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
-
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
         )
-
         with context.begin_transaction():
             context.run_migrations()
 
--- electricity-bot-backend/application/models.py
+++ electricity-bot-backend/application/models.py
@@ -1,7 +1,16 @@
-from sqlalchemy import Column, String, Boolean, DateTime
+from sqlalchemy import Column, String, Boolean, DateTime, Table, ForeignKey
+from sqlalchemy.orm import relationship
 from application.database import base
 import uuid
 from datetime import datetime, timezone
+
+
+user_device_association = Table(
+    "user_device_association", # for many-to-many relationship
+    base.metadata,
+    Column("user_id", String(255), ForeignKey("users.user_id", ondelete="CASCADE")),
+    Column("device_id", String(255), ForeignKey("devices.device_id", ondelete="CASCADE")),
+)
 
 # ORM models
 
@@ -12,10 +21,14 @@ class DeviceModel(base):
     device_id = Column(
         String(255), primary_key=True, default=lambda: str(uuid.uuid4()), nullable=False
     )
-    owner_id = Column(String(255), default=lambda: str(uuid.uuid4()), nullable=False)
-    owner_email = Column(String(255), nullable=False, unique=True)
     last_seen = Column(
         DateTime, default=lambda: datetime.now(timezone.utc), nullable=False
+    )
+
+    users = relationship(
+        "UserModel",
+        secondary=user_device_association,
+        back_populates="devices"
     )
 
 
@@ -25,7 +38,7 @@ class MeasurementModel(base):
     measurement_id = Column(
         String(255), primary_key=True, default=lambda: str(uuid.uuid4()), nullable=False
     )
-    device_id = Column(String(255), default=lambda: str(uuid.uuid4()), nullable=False)
+    device_id = Column(String(255), nullable=False)
     timestamp = Column(
         DateTime, default=lambda: datetime.now(timezone.utc), nullable=False
     )
@@ -41,3 +54,16 @@ class UserModel(base):
     email = Column(String(255), nullable=False, unique=True)
     first_name = Column(String(255), nullable=False)
     last_name = Column(String(255), nullable=False)
+
+    devices = relationship(
+        "DeviceModel",
+        secondary=user_device_association,
+        back_populates="users"
+    )
+
+
+class UnassignedDeviceModel(base): 
+    __tablename__ = "unassigned_devices" # table for devices not assigned to any user
+    # device_id is moved there when device was delited, or user was deleted and device with them too
+
+    device_id = Column(String(255), primary_key=True, nullable=False)
