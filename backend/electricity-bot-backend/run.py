aeba626aa43461d893821024058c3cc553e451ac
olesia-mykhailyshyn <omykhailyshyn@kse.org.ua>
2025-06-17 12:17:47 +0300

    move backend code into separate directory for better structure

--- backend/electricity-bot-backend/run.py
+++ backend/electricity-bot-backend/run.py
@@ -4,4 +4,4 @@ from application.background_tasks import start_scheduler
 start_scheduler()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
