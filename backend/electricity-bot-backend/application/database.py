e06acfd36d1152630fc376a412fafbdbc4f62184
olesia-mykhailyshyn <omykhailyshyn@kse.org.ua>
2025-06-18 01:08:39 +0300

    lint was fixed

--- backend/electricity-bot-backend/application/database.py
+++ backend/electricity-bot-backend/application/database.py
@@ -5,7 +5,7 @@ DATABASE_URL = "mysql+pymysql://user:88888888@mysql:3306/electricity_bot_bd"
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True,  
+    pool_pre_ping=True,
 )
 
 session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
