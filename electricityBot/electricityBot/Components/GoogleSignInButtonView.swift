ef6a4a0cbd22e28be4ad77cedb7360a7199f99bb
Dana Litvak <dlitvakk21@gmail.com>
2025-06-18 21:40:23 +0300

    organised the structrure of project
    - decomposed GoogleSignInButtonView to UI and Network Layers

--- electricityBot/electricityBot/Components/GoogleSignInButtonView.swift
+++ electricityBot/electricityBot/Components/GoogleSignInButtonView.swift
@@ -49,59 +49,23 @@ struct GoogleSignInButtonView: View {
             
             // receive Google ID token and send it to the server
             guard let idToken = result.user.idToken?.tokenString else {
-                        print("No idToken found")
-                        return
-                    }
+                print("No idToken found")
+                return
+            }
 
-            sendTokenToBackend(idToken: idToken)
+            GoogleSignIn.sendTokenToBackend(idToken: idToken) { result in
+                switch result {
+                    case .success(let user):
+                        DispatchQueue.main.async {
+                            self.userSession.user = user
+                            self.userSession.isLoggedIn = true
+                    }
+                    case .failure(let error):
+                        print("Backend auth failed:", error.localizedDescription)
+                }
+            }
         }
     }
-    
-    func sendTokenToBackend(idToken: String) {
-        // server endpoint
-        guard let url = URL(string: "http://127.0.0.1:5000/api/auth/login") else { return }
-
-        // JSON POST request
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-
-        // packs the data in JSON format to send
-        let body: [String: Any] = ["id_token": idToken]
-        request.httpBody = try? JSONSerialization.data(withJSONObject: body)
-
-        // sends request to server
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            // handles errors
-            if let error = error {
-                print("Request failed:", error.localizedDescription)
-                return
-            }
-
-            if let httpResponse = response as? HTTPURLResponse {
-                print("Backend response code:", httpResponse.statusCode)
-            }
-
-            guard let data = data else {
-                print("No data in response")
-                return
-            }
-
-            do {
-                let json = try JSONDecoder().decode(User.self, from: data)
-                        
-                // create a current user profile
-                DispatchQueue.main.async {
-                    self.userSession.user = json
-                    self.userSession.isLoggedIn = true
-                }
-            } catch {
-                print("Failed to decode JSON:", error.localizedDescription)
-            }
-            
-        }.resume()
-    }
-
 }
 
 #Preview {
